0c2138f524d982c6882f80c316beee9fe6ae6bdd
Avneet kaur <avneetk9181@gmail.com>
2026-07-23 13:30:51 +0530

    Time: 0 ms (100%), Space: 28.2 MB (70.07%) - LeetHub - Time: O(n), Space: O(1)

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int count=1,maxCount=0,el=nums[0];
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++){
-            if(nums[i]!=nums[i-1]){
-                count=1;
-            }else{
-                count++;
+        // int n=nums.size();
+        // int count=1,maxCount=0,el=nums[0];
+        // sort(nums.begin(),nums.end());
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]!=nums[i-1]){
+        //         count=1;
+        //     }else{
+        //         count++;
+        //     }
+        //     if(count>n/2){
+        //         el=nums[i];
+        //         break;
+        //     }
+        // }
+        // return el;
+
+        //by MOORE'S VOTING ALGORITHM
+        int freq=0,el=0;
+        for(int num : nums){
+            if(freq==0){
+                el=num;
             }
-            maxCount=max(maxCount,count);
-            if(maxCount>n/2){
-                el=nums[i];
-                break;
+            if(el==num){
+                freq++;
+            }else{
+                freq--;
             }
         }
         return el;
