4beb4113e0ecf6fea9098b98bc39a95bab72b4ba
Avneet kaur <avneetk9181@gmail.com>
2026-07-24 23:01:51 +0530

    Time: O(n), Space: O(1)

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorResult=0;
+        for(int num:nums){
+            xorResult^=num;
+        }
+        return xorResult;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
