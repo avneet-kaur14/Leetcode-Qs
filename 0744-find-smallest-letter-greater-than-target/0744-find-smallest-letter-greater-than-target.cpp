189b315f04b0784b496a935d0f82bf136f16e00e
Avneet kaur <avneetk9181@gmail.com>
2026-08-03 23:53:15 +0530

    Time: O(log n), Space: O(1)

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        int i=0;
-        while(i<letters.size()){
-            if(letters[i]>target){
-                return letters[i];
+        // int i=0;
+        // while(i<letters.size()){
+        //     if(letters[i]>target){
+        //         return letters[i];
+        //     }
+        //     i++;
+        // }
+        // return letters[0];
+
+        int st=0,end=letters.size()-1,ans=letters[0];
+
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(letters[mid]>target){
+                ans=letters[mid];
+                end=mid-1;
+            }else{
+                st=mid+1;
             }
-            i++;
         }
-        return letters[0];
+        return ans;
     }
 };
 
