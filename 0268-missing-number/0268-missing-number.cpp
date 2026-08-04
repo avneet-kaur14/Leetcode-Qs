bdb4dd7f814a8da3500b6998f5527ee08128b55a
Avneet kaur <avneetk9181@gmail.com>
2026-08-04 22:17:14 +0530

    Time: O(n), Space: O(1)

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        //using xor method-duplicates cancel out
+        int ans=nums.size();
+        for(int i=0;i<nums.size();i++){
+            ans^=i;
+            ans^=nums[i];
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
