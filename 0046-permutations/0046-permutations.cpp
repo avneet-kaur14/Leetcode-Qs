df540edb4a6ca7ed34ed059ea5794bc568912f63
Avneet kaur <avneetk9181@gmail.com>
2026-08-14 23:50:43 +0530

    Time: O(N * N!), Space: O(N * N!)

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void solve(vector<int>&nums,int index,vector<vector<int>>& result){
+        if(index==nums.size()){
+            result.push_back(nums);
+            return;
+        }
+
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            solve(nums,index+1,result);
+            swap(nums[index],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        solve(nums,0,result);
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
