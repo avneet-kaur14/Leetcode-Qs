f6b0c28e85443c1fa14c8f4ac31dbbf2a8641f85
Avneet kaur <avneetk9181@gmail.com>
2026-09-14 16:28:50 +0530

    O(n), Space: O(1)

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string og="";
-        string temp="";
-        // int k=0;
-        for(int i=0;i<=s.length()-1;i++){
-            if(isalnum(s[i])){
-                og+=tolower(s[i]);
+        // string og="";
+        // string temp="";
+        // // int k=0;
+        // for(int i=0;i<=s.length()-1;i++){
+        //     if(isalnum(s[i])){
+        //         og+=tolower(s[i]);
+        //     }
+        // }
+        // for(int i=og.length()-1;i>=0;i--){
+        //     temp+=og[i];
+        // }
+        // if(temp==og){
+        //     return true;
+        // }
+        // return false;
+
+
+        int left=0,right=s.length()-1;
+        while(left<right){
+            while(left<right && !isalnum(s[left])){
+                left++;
             }
+            while(left<right && !isalnum(s[right])){
+                right--;
+            }
+            if(tolower(s[left])!=tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
         }
-        for(int i=og.length()-1;i>=0;i--){
-            temp+=og[i];
-        }
-        if(temp==og){
-            return true;
-        }
-        return false;
+        return true;
     }
 };
 
